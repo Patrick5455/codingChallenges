653b9085a123e6eb843bb3e6bcae05e4342ba682
Patrick OJunde <patrickojunde@gmail.com>
2020-02-29 07:17:30 +0100

    This Folder contains the Test Codes

---
+++ testFolder/games/TicTacToeTest.java
@@ -0,0 +1,38 @@
+package games;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setArrayValue() {;
+    }
+
+    @Test
+    void checkHorizontalWin() {
+    }
+
+    @Test
+    void checkLeftVerticalWin() {
+    }
+
+    @Test
+    void displayBox() {
+    }
+
+    @Test
+    void determineWins() {
+    }
+}
